a6f709736f34cd852dd7bb1eaeb807476f2266db
Shintaro Sakoda <sakoda@keio.jp>
2023-02-05 11:31:55 +0900

    入力読み取り部分を追加

--- cpp/main.cpp
+++ cpp/main.cpp
@@ -6,9 +6,29 @@
 
 int main() {
   std::cout << "NeRF Practice" << std::endl;
-  cv::Mat img = cv::imread("/root/nerf_practice/data/test/greek/rgb/00000.png");
-  if (img.empty()) {
-    return 1;
-  }
-  cv::imwrite("qwe.png", img);
+  const std::string dataset_path = "../data/train/greek/";
+
+  std::ifstream ifs_intrinsics(dataset_path + "intrinsics.txt");
+
+  // 1行目
+  float f, cx, cy, _;
+  ifs_intrinsics >> f >> cx >> cy >> _;
+
+  // 2行目
+  float origin_x, origin_y, origin_z;
+  ifs_intrinsics >> origin_x >> origin_y >> origin_z;
+
+  // 3行目
+  float near_plane;
+  ifs_intrinsics >> near_plane >> _;
+
+  // 4行目
+  float img_height, img_width;
+  ifs_intrinsics >> img_height >> img_width;
+
+  std::cout << "focal_length: " << f << std::endl;
+  std::cout << "image_center: "
+            << "(" << cx << ", " << cy << ")" << std::endl;
+  std::cout << "image size  : "
+            << "(" << img_height << ", " << img_width << ")" << std::endl;
 }
