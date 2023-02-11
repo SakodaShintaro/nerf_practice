779c6a87e8ce2f792617c39c5eceb40425a24ddf
Shintaro Sakoda <sakoda@keio.jp>
2023-02-11 14:36:49 +0900

    Poseの入力部分を修正

--- cpp/data.cpp
+++ cpp/data.cpp
@@ -5,10 +5,10 @@
 Pose ParsePose(const std::string& path) {
   std::ifstream ifs(path);
   Pose pose;
-  for (int32_t i = 0; i < 16; i++) {
-    float v;
-    ifs >> v;
-    pose << v;
+  for (int32_t i = 0; i < 4; i++) {
+    for (int32_t j = 0; j < 4; j++) {
+      ifs >> pose(i, j);
+    }
   }
   return pose;
 }
