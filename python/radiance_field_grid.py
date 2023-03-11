663acb93ac4c9315b3081aea53288e1e99b9e870
Shintaro Sakoda <sakoda@keio.jp>
2023-03-11 16:13:08 +0900

    不要なコードを削除

--- python/radiance_field_grid.py
+++ python/radiance_field_grid.py
@@ -1,34 +1,16 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import numpy as np
-from positional_encoder_grid2 import HashEmbedder
 from typing import Tuple
 from positional_encoder_grid import PositionalEncoderGrid
+from positional_encoder_grid2 import HashEmbedder
 
 
-def img2mse(x, y):
-    return torch.mean((x - y) ** 2)
-
-
-def mse2psnr(x):
-    return -10. * torch.log(x) / torch.log(torch.Tensor([10.]))
-
-
-def to8b(x):
-    return (255 * np.clip(x, 0, 1)).astype(np.uint8)
-
-
-def get_embedder(args, i=0):
-    embed = HashEmbedder(bounding_box=args.bounding_box,
-                         log2_hashmap_size=args.log2_hashmap_size,
-                         finest_resolution=args.finest_res)
-    out_dim = embed.out_dim
-    return embed, out_dim
-
-
-# Small NeRF for Hash embeddings
 class RadianceFieldGrid(nn.Module):
+    """ Multi resolution hash encodingを使ったradiance field
+    参考) https://github.com/yashbhalgat/HashNeRF-pytorch
+    """
+
     def __init__(self,
                  num_layers=3,
                  hidden_dim=64,
@@ -38,10 +20,8 @@ class RadianceFieldGrid(nn.Module):
                  ):
         super(RadianceFieldGrid, self).__init__()
 
-        # self.enc_pos = PositionalEncoderGrid()
-        # self.enc_dir = PositionalEncoderGrid()
-        self.enc_pos = HashEmbedder()
-        self.enc_dir = HashEmbedder()
+        self.enc_pos = PositionalEncoderGrid()
+        self.enc_dir = PositionalEncoderGrid()
 
         self.input_ch = self.enc_pos.encoded_dim()
         self.input_ch_views = self.enc_dir.encoded_dim()
@@ -107,96 +87,5 @@ class RadianceFieldGrid(nn.Module):
                 h = F.relu(h, inplace=True)
 
         color = torch.sigmoid(h)
-        # color = h
 
         return color, sigma
-
-
-# Ray helpers
-def get_rays(H, W, K, c2w):
-    i, j = torch.meshgrid(torch.linspace(0, W - 1, W), torch.linspace(0, H - 1, H))  # pytorch's meshgrid has indexing='ij'
-    i = i.t()
-    j = j.t()
-    dirs = torch.stack([(i - K[0][2]) / K[0][0], -(j - K[1][2]) / K[1][1], -torch.ones_like(i)], -1)
-    # Rotate ray directions from camera frame to the world frame
-    rays_d = torch.sum(dirs[..., np.newaxis, :] * c2w[:3, :3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
-    # Translate camera frame's origin to the world frame. It is the origin of all rays.
-    rays_o = c2w[:3, -1].expand(rays_d.shape)
-    return rays_o, rays_d
-
-
-def get_rays_np(H, W, K, c2w):
-    i, j = np.meshgrid(np.arange(W, dtype=np.float32), np.arange(H, dtype=np.float32), indexing='xy')
-    dirs = np.stack([(i - K[0][2]) / K[0][0], -(j - K[1][2]) / K[1][1], -np.ones_like(i)], -1)
-    # Rotate ray directions from camera frame to the world frame
-    rays_d = np.sum(dirs[..., np.newaxis, :] * c2w[:3, :3], -1)  # dot product, equals to: [c2w.dot(dir) for dir in dirs]
-    # Translate camera frame's origin to the world frame. It is the origin of all rays.
-    rays_o = np.broadcast_to(c2w[:3, -1], np.shape(rays_d))
-    return rays_o, rays_d
-
-
-def ndc_rays(H, W, focal, near, rays_o, rays_d):
-    # Shift ray origins to near plane
-    t = -(near + rays_o[..., 2]) / rays_d[..., 2]
-    rays_o = rays_o + t[..., None] * rays_d
-
-    # Projection
-    o0 = -1. / (W / (2. * focal)) * rays_o[..., 0] / rays_o[..., 2]
-    o1 = -1. / (H / (2. * focal)) * rays_o[..., 1] / rays_o[..., 2]
-    o2 = 1. + 2. * near / rays_o[..., 2]
-
-    d0 = -1. / (W / (2. * focal)) * (rays_d[..., 0] / rays_d[..., 2] - rays_o[..., 0] / rays_o[..., 2])
-    d1 = -1. / (H / (2. * focal)) * (rays_d[..., 1] / rays_d[..., 2] - rays_o[..., 1] / rays_o[..., 2])
-    d2 = -2. * near / rays_o[..., 2]
-
-    rays_o = torch.stack([o0, o1, o2], -1)
-    rays_d = torch.stack([d0, d1, d2], -1)
-
-    return rays_o, rays_d
-
-
-# Hierarchical sampling (section 5.2)
-def sample_pdf(bins, weights, N_samples, det=False, pytest=False):
-    # Get pdf
-    weights = weights + 1e-5  # prevent nans
-    pdf = weights / torch.sum(weights, -1, keepdim=True)
-    cdf = torch.cumsum(pdf, -1)
-    cdf = torch.cat([torch.zeros_like(cdf[..., :1]), cdf], -1)  # (batch, len(bins))
-
-    # Take uniform samples
-    if det:
-        u = torch.linspace(0., 1., steps=N_samples)
-        u = u.expand(list(cdf.shape[:-1]) + [N_samples])
-    else:
-        u = torch.rand(list(cdf.shape[:-1]) + [N_samples])
-
-    # Pytest, overwrite u with numpy's fixed random numbers
-    if pytest:
-        np.random.seed(0)
-        new_shape = list(cdf.shape[:-1]) + [N_samples]
-        if det:
-            u = np.linspace(0., 1., N_samples)
-            u = np.broadcast_to(u, new_shape)
-        else:
-            u = np.random.rand(*new_shape)
-        u = torch.Tensor(u)
-
-    # Invert CDF
-    u = u.contiguous()
-    inds = torch.searchsorted(cdf, u, right=True)
-    below = torch.max(torch.zeros_like(inds - 1), inds - 1)
-    above = torch.min((cdf.shape[-1] - 1) * torch.ones_like(inds), inds)
-    inds_g = torch.stack([below, above], -1)  # (batch, N_samples, 2)
-
-    # cdf_g = tf.gather(cdf, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
-    # bins_g = tf.gather(bins, inds_g, axis=-1, batch_dims=len(inds_g.shape)-2)
-    matched_shape = [inds_g.shape[0], inds_g.shape[1], cdf.shape[-1]]
-    cdf_g = torch.gather(cdf.unsqueeze(1).expand(matched_shape), 2, inds_g)
-    bins_g = torch.gather(bins.unsqueeze(1).expand(matched_shape), 2, inds_g)
-
-    denom = (cdf_g[..., 1] - cdf_g[..., 0])
-    denom = torch.where(denom < 1e-5, torch.ones_like(denom), denom)
-    t = (u - cdf_g[..., 0]) / denom
-    samples = bins_g[..., 0] + t * (bins_g[..., 1] - bins_g[..., 0])
-
-    return samples
