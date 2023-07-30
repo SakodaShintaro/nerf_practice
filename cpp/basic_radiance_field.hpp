528c0d00f2b404d8fea95f9bfb3b6ae53883a4c1
Shintaro Sakoda <sakoda@keio.jp>
2023-07-30 17:16:16 +0900

    Added switching
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ cpp/basic_radiance_field.hpp
@@ -0,0 +1,36 @@
+#ifndef BASIC_RADIANCE_FIELD_HPP
+#define BASIC_RADIANCE_FIELD_HPP
+
+#include <torch/torch.h>
+
+class BasicRadianceFieldImpl : public torch::nn::Module {
+ public:
+  BasicRadianceFieldImpl(const int32_t _L_x = 10, const int32_t _L_d = 4);
+
+  std::pair<torch::Tensor, torch::Tensor> forward(torch::Tensor x, torch::Tensor d);
+
+ private:
+  torch::Tensor gamma(torch::Tensor p, int32_t L);
+
+  const int32_t L_x;
+  const int32_t L_d;
+  torch::nn::Linear layer0_{nullptr};
+  torch::nn::Linear layer1_{nullptr};
+  torch::nn::Linear layer2_{nullptr};
+  torch::nn::Linear layer3_{nullptr};
+  torch::nn::Linear layer4_{nullptr};
+  torch::nn::Linear layer5_{nullptr};
+  torch::nn::Linear layer6_{nullptr};
+  torch::nn::Linear layer7_{nullptr};
+  torch::nn::Linear sigma_{nullptr};
+  torch::nn::Linear layer8_{nullptr};
+  torch::nn::Linear layer9_{nullptr};
+  torch::nn::Linear layer10_{nullptr};
+  torch::nn::Linear layer11_{nullptr};
+  torch::nn::Linear layer12_{nullptr};
+  torch::nn::Linear rgb_{nullptr};
+};
+
+TORCH_MODULE(BasicRadianceField);
+
+#endif
--- cpp/radiance_field.hpp
+++ cpp/radiance_field.hpp
@@ -1,36 +1,5 @@
-#ifndef RADIANCE_FIELD_HPP
-#define RADIANCE_FIELD_HPP
+#pragma once
 
-#include <torch/torch.h>
+#include "basic_radiance_field.hpp"
 
-class RadianceFieldImpl : public torch::nn::Module {
- public:
-  RadianceFieldImpl(const int32_t _L_x = 10, const int32_t _L_d = 4);
-
-  std::pair<torch::Tensor, torch::Tensor> forward(torch::Tensor x, torch::Tensor d);
-
- private:
-  torch::Tensor gamma(torch::Tensor p, int32_t L);
-
-  const int32_t L_x;
-  const int32_t L_d;
-  torch::nn::Linear layer0_{nullptr};
-  torch::nn::Linear layer1_{nullptr};
-  torch::nn::Linear layer2_{nullptr};
-  torch::nn::Linear layer3_{nullptr};
-  torch::nn::Linear layer4_{nullptr};
-  torch::nn::Linear layer5_{nullptr};
-  torch::nn::Linear layer6_{nullptr};
-  torch::nn::Linear layer7_{nullptr};
-  torch::nn::Linear sigma_{nullptr};
-  torch::nn::Linear layer8_{nullptr};
-  torch::nn::Linear layer9_{nullptr};
-  torch::nn::Linear layer10_{nullptr};
-  torch::nn::Linear layer11_{nullptr};
-  torch::nn::Linear layer12_{nullptr};
-  torch::nn::Linear rgb_{nullptr};
-};
-
-TORCH_MODULE(RadianceField);
-
-#endif
+using RadianceField = BasicRadianceField;
