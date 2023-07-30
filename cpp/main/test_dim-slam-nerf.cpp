af4dd031c6486404438c9d5c8fb13865af0e9c01
Shintaro Sakoda <sakoda@keio.jp>
2023-07-30 14:43:00 +0900

    Added test_dim-slam-nerf.cpp
    Signed-off-by: Shintaro Sakoda <sakoda@keio.jp>

---
+++ cpp/main/test_dim-slam-nerf.cpp
@@ -0,0 +1,39 @@
+#include <filesystem>
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <random>
+
+#include "../data.hpp"
+#include "../dim-slam-nerf/nerf.hpp"
+#include "../nerf_function.hpp"
+#include "../timer.hpp"
+#include "../utils.hpp"
+
+void TestGetRays() {
+  const std::string dataset_path = "/home/sakoda/data/converted/logiee/20230609_base_link_logiee/";
+  CameraIntrinsicParameter param = GetCameraIntrinsicParameter(dataset_path);
+  const std::vector<std::string> pose_paths = Glob(dataset_path + "pose/");
+  const std::vector<std::string> image_paths = Glob(dataset_path + "images/");
+  assert(pose_paths.size() == image_paths.size());
+  const int32_t N = pose_paths.size();
+  std::vector<Data> dataset_raw(N);
+  const int32_t i = 0;
+  std::cout << pose_paths[i] << " " << image_paths[i] << std::endl;
+  dataset_raw[i].pose = ParsePose(pose_paths[i]);
+  dataset_raw[i].image = cv::imread(image_paths[i]);
+  std::vector<RayData> curr_rays = GetRays(param, dataset_raw[i]);
+
+  curr_rays.erase(curr_rays.begin() + 1, curr_rays.end());
+
+  auto [o, d, C] = RayData2Tensor(curr_rays);
+  std::cout << "o = " << o << std::endl;
+  std::cout << "d = " << d << std::endl;
+  std::cout << "C = " << C << std::endl;
+
+  NeRF nerf;
+  auto [C_c, C_f] = nerf->forward(o, d);
+  std::cout << "C_c = " << C_c << std::endl;
+  std::cout << "C_f = " << C_f << std::endl;
+}
+
+int main() { TestGetRays(); }
